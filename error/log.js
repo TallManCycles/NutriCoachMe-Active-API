6c37dab34855c7292c4b933d76e3b689d1d84651
Aaron <aaronday@live.com.au>
2024-11-09 22:07:11 +1000

    #60 Wrap the error logging in a try catch

--- error/log.js
+++ error/log.js
@@ -1,37 +1,43 @@
 import fs from 'fs';
 
 function logError(error) {
-    
-    if (process.env.NODE_ENV === 'development') {
-        console.error(error);
-        return;
-    }
-    
-    const timestamp = new Date().toISOString();
-    const errorMessage = `${timestamp} - ${error.message}\n`;
-
-    fs.appendFile('error_log.txt', errorMessage, (err) => {
-        if (err) {
-            console.error('Failed to write to log file:', err);
+    try {
+        if (process.env.NODE_ENV === 'development') {
+            console.error(error);
+            return;
         }
-    });
+
+        const timestamp = new Date().toISOString();
+        const errorMessage = `${timestamp} - ${error.message}\n`;
+
+        fs.appendFile('error_log.txt', errorMessage, (err) => {
+            if (err) {
+                console.error('Failed to write to log file:', err);
+            }
+        });        
+    } catch (err) {
+        console.error('Failed to log error:', err);
+    }
 }
 
 function logInfo(customInfo) {
-
-    if (process.env.NODE_ENV === 'development') {
-        console.log(customInfo);
-        return;
-    }
-    
-    const timestamp = new Date().toISOString();
-    const logMessage = `${timestamp} - ${customInfo}\n`;
-
-    fs.appendFile('error_log.txt', logMessage, (err) => {
-        if (err) {
-            console.error('Failed to write to log file:', err);
+    try {
+        if (process.env.NODE_ENV === 'development') {
+            console.log(customInfo);
+            return;
         }
-    });
+
+        const timestamp = new Date().toISOString();
+        const logMessage = `${timestamp} - ${customInfo}\n`;
+
+        fs.appendFile('error_log.txt', logMessage, (err) => {
+            if (err) {
+                console.error('Failed to write to log file:', err);
+            }
+        });        
+    } catch (err) {
+        console.error('Failed to log info:', err);
+    }    
 }
 
 export { logError, logInfo};
