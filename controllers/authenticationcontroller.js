dd65eebec241efedab19220d5bc6c947480d2232
Aaron <aaronday@live.com.au>
2024-12-21 08:53:07 +1000

    Update

--- controllers/authenticationcontroller.js
+++ controllers/authenticationcontroller.js
@@ -5,13 +5,19 @@ import {logError} from "../error/log.js";
 const authenticate = async (req, res, next) => {
 
     const token = req.headers.authorization?.split(' ')[1];
+    
+    let secret = process.env.SUPABASE_JWT_SECRET;
+    
+    if (process.env.NODE_ENV === 'development') {
+        process.env.SUPABASE_TEST_JWT_SECRET;
+    }
 
     if (!token) {
         return res.status(401).json({ error: 'Unauthorized' });
     }
 
     try {
-        req.user = jwt.verify(token, process.env.NODE_ENV === 'development' ? process.env.SUPABASE_TEST_JWT_SECRET : process.env.SUPABASE_JWT_SECRET);
+        req.user = jwt.verify(token,secret );
         next();
     } catch (error) {
         logError(error);
